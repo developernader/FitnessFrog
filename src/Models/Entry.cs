9373da4028c7199bf4adc091e9fc0f01fbe4b223
Nader Saoudy <developernader@gmail.com>
2022-10-10 00:45:02 +0300

    Updated the Intensity field to use radio buttons

--- src/Models/Entry.cs
+++ src/Models/Entry.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using FitnessFrog.Data;
+using static FitnessFrog.Models.Activity;
 
 namespace FitnessFrog.Models
 {
@@ -13,30 +14,34 @@ namespace FitnessFrog.Models
         {
 
         }
+
         public enum IntensityLevel
         {
             Low,
             Medium,
             High
         }
+
         public int Id { get; set; }
         public DateTime Date { get; set; }
 
         [Display(Name = "Activity")]
         public int ActivityId { get; set; }
+        public Activity Activity { get; set; }
         public string Notes { get; set; }
         public double Duration { get; set; }
         public bool Exclude { get; set; }
-        public int Intensity { get; set; }
 
-        public Entry(int id, int year, int month, int day, Activity activity,
-            double duration, IntensityLevel intensityLevel, bool exclude = false, string notes = null)
+        public IntensityLevel Intensity { get; set; }
+
+        public Entry(int id, int year, int month, int day, ActivityType activityType,
+                    double duration, IntensityLevel intensity = IntensityLevel.Medium, bool exclude = false, string notes = null)
         {
             Id = id;
             Date = new DateTime(year, month, day);
             ActivityId = ActivityId;
             Duration = duration;
-            //IntensityLevel = (int)intensityLevel;
+            //IntensityLevel = (int)IntensityLevel;
             Exclude = exclude;
             Notes = notes;
         }
