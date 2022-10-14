065a041e233db1efc7fa81e2295f9943589180bb
Nader Saoudy <developernader@gmail.com>
2022-10-14 23:28:35 +0300

    Updated the controller to support the "Edit Entry" page

--- src/Controllers/EntriesController.cs
+++ src/Controllers/EntriesController.cs
@@ -46,40 +46,79 @@ namespace FitnessFrog.Controllers
 
             };
 
-            ViewBag.ActivitiesSelectListItems = new SelectList(
-                Data.Data.Activities, "Id", "Name");
+            SetupActivitiesSelectListItems();
             return View();
         }
 
         [HttpPost]
         public ActionResult Add(Entry entry)
         {
+            ValidateEntry(entry);
+
             if (ModelState.IsValid)
             {
                 _entriesRepository.AddEntry(entry);
-
                 return RedirectToAction("Index");
             }
 
-            ViewBag.ActivitiesSelectListItems = new SelectList(
-      Data.Data.Activities, "Id", "Name");
+            SetupActivitiesSelectListItems();
 
             return View(entry);
         }
+
+       
 
         public ActionResult Edit(int? id)
         {
             if (id is null)
                 return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
 
+            Entry entry = _entriesRepository.GetEntry((int)id);
+            if (entry == null)
+            {
+                return HttpNotFound();
+            }
+
+            SetupActivitiesSelectListItems();
+
+            return View(entry);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Entry entry)
+        {
+            ValidateEntry(entry);
+
+            if (ModelState.IsValid)
+            {
+                _entriesRepository.UpdateEntry(entry);
+                return RedirectToAction("Index");
+            }
+            
+            SetupActivitiesSelectListItems();
+
             return View();
         }
+
         public ActionResult Delete(int? id)
         {
             if (id is null)
                 return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
 
             return View();
+        }
+
+        public void ValidateEntry(Entry entry)
+        {
+            if (ModelState.IsValidField("Duration") && entry.Duration <= 0)
+            {
+                ModelState.AddModelError("Duration", "The Duration filed value must  be greater than '0'. ");
+            }
+        }
+        private void SetupActivitiesSelectListItems()
+        {
+            ViewBag.ActivitiesSelectListItems = new SelectList(
+                                    Data.Data.Activities, "Id", "Name");
         }
     }
 }
